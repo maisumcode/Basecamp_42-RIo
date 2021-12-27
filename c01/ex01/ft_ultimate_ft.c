16a4e3670837ee0f7386c587218dde62cdb1552b
igor <igorrturano@@gmail.com>
2021-12-27 01:44:59 -0300

    ex01

--- c01/ex01/ft_ultimate_ft.c
+++ c01/ex01/ft_ultimate_ft.c
@@ -1,9 +1,25 @@
-int ft_ultimate_ft(int *********nbr)
+void ft_ultimate_ft(int *********nbr)
 {
     *********nbr = 42;
 }
 
+
+// #include <stdio.h>
 // int main(void)
 // {
+//     int answer;
+//     answer = 0;
+//     printf("%i\n", answer);
+//     int *point = &answer;
+//     int **point1 = &point;
+//     int ***point2 = &point1;
+//     int ****point3 = &point2;
+//     int *****point4 = &point3;
+//     int ******point5 = &point4;
+//     int *******point6 = &point5;
+//     int ********point7 = &point6;
+//     int *********point8 = &point7;
 
+//     ft_ultimate_ft(point8);
+//     printf("%i", answer);
 // }
