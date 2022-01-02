33c2b0f40139453f2cd7b6cc505a0f6919510c12
igor <igorrturano@gmail.com>
2022-01-02 13:47:58 -0300

    ex07

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -1,30 +1,32 @@
-#include <unistd.h>
-
-void    ft_putchar(char *c)
-{
-    write(1, &c, 1);
-}
-
 void    ft_rev_int_tab(int *tab, int size)
 {
-    int hold_values; 
+    int hold_values;
     int i;
     i = 0;
-    
-    while(i < (size / 2)) 
+
+    while(i < (size / 2))
     {
         hold_values = tab[i];
         tab[i] = tab[size - 1 - i];
         tab[size - 1 - i] = hold_values;
-        ft_putchar(tab[size - 1 - i] + '0');
         i++;
     }
 }
 
-int main(void)
-{
-    int value[2] = { 4, 2 };
-    int *ptr = &value;
+// #include <stdio.h>
+// void print_array(int *arr)
+// {
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%i, ", arr[i]);
+//     }
+// }
 
-    ft_rev_int_tab(ptr, 2);
-}
+// int main(void)
+// {
+//     int value[] = { 4, 2, 3, 5 };
+//     int *ptr = value;
+//     print_array(value);
+//     ft_rev_int_tab(ptr, 4);
+//     print_array(value);
+// }
