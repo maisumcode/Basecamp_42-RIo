2fcabe17430440bc0c8306d5f91f6a845a07b152
igor <igorrturano@gmail.com>
2022-01-06 00:34:13 -0300

    ex02 working properly

--- c02/ex02/ft_str_is_alpha.c
+++ c02/ex02/ft_str_is_alpha.c
@@ -1,23 +1,31 @@
 int ft_str_is_alpha(char *str)
 {
     int i;
+    int result;
 
     i = 0;
+
+    if (str[0] == '\0')
+        return 1;
+
     while (str[i])
     {
-        if (str[i] > 'a' || str[i] < 'z' || str[i] > 'A' || str[i] < 'Z' || str[0] == '\0')
-        {
-            return 1;
-        }
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+            result = 1;
+        else
+            result = 0;
+
+        if (result == 0)
+            return result;
         i++;
     }
-    return 0;
+    return result;
 }
 
 #include <stdio.h>
 int main(void)
 {
-    printf("%i\n", ft_str_is_alpha("Olá"));
+    printf("%i\n", ft_str_is_alpha("Olá1"));
     printf("%i\n", ft_str_is_alpha("123"));
     printf("%i\n", ft_str_is_alpha(""));
 }
