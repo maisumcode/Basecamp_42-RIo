54329259659992c69753a04630363f3de6d440e1
igor <igorrturano@gmail.com>
2022-01-06 01:18:56 -0300

    ex06

---
+++ c02/ex06/ft_str_is_printable.c
@@ -0,0 +1,20 @@
+int ft_str_is_printable(char *str)
+{
+	while (*str != '\0')
+	{
+		if (!(str[0] >= 32 && str[0] <= 126))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+#include <stdio.h>
+int main(void)
+{
+    printf("%i\n", ft_str_is_printable("ä1"));
+    printf("%i\n", ft_str_is_printable("{123}"));
+    printf("%i\n", ft_str_is_printable(""));
+    printf("%i\n", ft_str_is_printable("AAA"));
+    printf("%i\n", ft_str_is_printable("aaa"));
+}
